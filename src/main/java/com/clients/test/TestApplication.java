35cb98a333be06cbc259057b9110d28c88364701
sandrine-bd <sandrine.barrucand@le-campus-numerique.fr>
2025-11-03 12:17:32 +0100

    Bean partagé de RestTemplate

--- src/main/java/com/clients/test/TestApplication.java
+++ src/main/java/com/clients/test/TestApplication.java
@@ -2,6 +2,8 @@ package com.clients.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -11,4 +13,8 @@ public class TestApplication {
 		SpringApplication.run(TestApplication.class, args);
 	}
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 }
--- src/main/java/com/clients/test/web/service/LicenseValidationService.java
+++ src/main/java/com/clients/test/web/service/LicenseValidationService.java
@@ -10,9 +10,13 @@ import java.util.Map;
 public class LicenseValidationService {
 
     private static final String LICENSE_API_URL = "http://localhost:8081/licenses";
+    private final RestTemplate restTemplate;
+
+    public LicenseValidationService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public boolean isLicenseValid(String licenseNumber) {
-        RestTemplate restTemplate = new RestTemplate();
         try {
             ResponseEntity<Map> response = restTemplate.getForEntity(LICENSE_API_URL + licenseNumber, Map.class);
             if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
